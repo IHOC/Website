913a3733dadd2d7b69ecff56cf901df00222a653
Derek Jacobs <webfox@bluevulpine.net>
2016-05-21 19:16:59 -0500

    Add Youtube and Streaming page. New members can be added by editing the members.json file.

--- TeamIHOC/Controllers/HomeController.cs
+++ TeamIHOC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -41,5 +43,30 @@ namespace TeamIHOC.Controllers
 	    {
 	        return View();
 	    }
-	}
+
+        public ActionResult Youtube()
+        {
+            List<Models.MemberInfo> members;
+            using (StreamReader r = new StreamReader(Server.MapPath("~/Content/members.json")))
+            {
+                string json = r.ReadToEnd();
+                members = JsonConvert.DeserializeObject<List<Models.MemberInfo>>(json);
+            }
+
+            // Put members with pictures before members without. Alphabetize both groups
+            members.Sort((a,b) =>
+            {
+                if (String.IsNullOrEmpty(a.ImageURL) && !String.IsNullOrEmpty(b.ImageURL))
+                    return 1;
+                else if (!String.IsNullOrEmpty(a.ImageURL) && String.IsNullOrEmpty(b.ImageURL))
+                    return -1;
+                else
+                {
+                    return String.Compare(a.Name, b.Name);
+                }
+            });
+
+            return View(members);
+        }
+    }
 }
---
+++ TeamIHOC/Models/MemberInfoModels.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TeamIHOC.Models
+{
+    public class MemberInfo
+    {
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public Dictionary<string, string> StreamingNames { get; set; }
+        public string Description { get; set; }
+    }
+}
